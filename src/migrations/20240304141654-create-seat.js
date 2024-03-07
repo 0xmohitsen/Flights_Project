4fa553bf3dfd50eef3ff8de5bc194811d0524bd2
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-08 00:24:43 +0530

    FIX: Changed the col type from int to string

--- src/migrations/20240304141654-create-seat.js
+++ src/migrations/20240304141654-create-seat.js
@@ -25,7 +25,7 @@ module.exports = {
         allowNull: false
       },
       col: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         allowNull: false
       },
       type: {
