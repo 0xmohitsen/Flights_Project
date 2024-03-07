98717233cbe297ebbbd6c71ea8edac463901a237
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-07 22:47:49 +0530

    FIX: correction in getTime function

--- src/utils/helper/dateTime-helper.js
+++ src/utils/helper/dateTime-helper.js
@@ -1,8 +1,8 @@
-function dateTime(arrivalTime, departureTime){
-    let time1 = new Date(arrivalTime);
-    let time2 = new Date(departureTime);
+function dateTime(arrivalTime, departureTime) {
+  let time1 = new Date(arrivalTime);
+  let time2 = new Date(departureTime);
 
-    return time1.getTime > time2.getTime;
+  return time1.getTime() > time2.getTime();
 }
 
 module.exports = dateTime;
